609a0d6c85267c646cdb18020eaacbe39d7c5895
Yoshika Takada <touch@rnsk.net>
2018-09-21 07:39:58 +0900

    音源再生の処理を追加

--- html/public/js/app.js
+++ html/public/js/app.js
@@ -3,26 +3,47 @@
  *
  * @copyright   Copyright (c) Yoshika
  * @author      Yoshika (@rnsk)
- * @link        http://rnsk.net/
+ * @link        https://rnsk.net/
  */
 
-/*---- WebSocket ----*/
+var connection,
+    connectionUrl = 'ws://localhost:5005',
+    audioElem,
+    audioFile = '../files/audio.mp3';
 
-var sock = new WebSocket('ws://localhost:5005');
+/*---- WebSocket ----*/
+connection = new WebSocket(connectionUrl);
 
 // 接続
-sock.addEventListener('open',function(e){
+connection.addEventListener('open', function (e) {
     console.log('Socket 接続成功');
 });
 
 // サーバーからデータを受け取る
-sock.addEventListener('message',function(e){
+connection.addEventListener('message', function (e) {
     console.log(e.data);
 });
 
-window.addEventListener('DOMContentLoaded',function(e){
+window.addEventListener('DOMContentLoaded', function (e) {
     // サーバーにデータを送る
-    document.getElementById('submit').addEventListener('click',function(e){
-        sock.send('hello');
+    document.getElementById('submit').addEventListener('click', function (e) {
+        connection.send('hello');
     });
+});
+
+/*---- 音源再生 ----*/
+audioElem = new Audio();
+audioElem.src = audioFile;
+
+document.getElementById('play').addEventListener('click', function (e) {
+    audioElem.play();
+});
+
+document.getElementById('pause').addEventListener('click', function (e) {
+    audioElem.pause();
+});
+
+document.getElementById('stop').addEventListener('click', function (e) {
+    audioElem.pause();
+    audioElem.currentTime = 0;
 });
